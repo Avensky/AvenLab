99116be1e4a2e836c47d3238c0087ede2aa60c7a
uriel <urielzacarias@gmail.com>
2026-07-03 22:03:42 -0700

    removed file from index

--- frontend/src/game/modes/index.ts
+++ frontend/src/game/modes/index.ts
@@ -3,5 +3,4 @@ export * from "./SignalRecon";
 export * from "./SignalReconMission";
 export * from "./Swarm";
 export * from "./SandboxSetup";
-export * from "./CanBusStatusBadge";
 export * from "./SignalReconSetup";
